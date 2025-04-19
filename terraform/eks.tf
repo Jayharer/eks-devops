3d174b92f5f937166511b44da058bbe8c037590e
jay415404 <jay416505@gmail.com>
2025-04-19 13:20:51 +0530

    feature: github action ecr push code

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,40 +1,40 @@
-# eks cluster
-resource "aws_eks_cluster" "particle41_eks" {
-  name     = "particle41_eks"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+# # eks cluster
+# resource "aws_eks_cluster" "particle41_eks" {
+#   name     = "particle41_eks"
+#   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  vpc_config {
-    subnet_ids = [aws_subnet.private_subet_1.id, aws_subnet.private_subet_2.id]
-  }
+#   vpc_config {
+#     subnet_ids = [aws_subnet.private_subet_1.id, aws_subnet.private_subet_2.id]
+#   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy,  
-    aws_iam_role_policy_attachment.eks_service_policy,
-  ]
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks_cluster_policy,  
+#     aws_iam_role_policy_attachment.eks_service_policy,
+#   ]
 
-  tags = {
-    Name = "particle41_eks"
-  }
-}
+#   tags = {
+#     Name = "particle41_eks"
+#   }
+# }
 
-# eks cluster
-resource "aws_eks_node_group" "particle41_eks_node_grop" {
-  cluster_name    = aws_eks_cluster.particle41_eks.name
-  node_group_name = "particle41_eks_node_grop"
-  node_role_arn   = aws_iam_role.eks_node_role.arn 
-  subnet_ids      = [aws_subnet.private_subet_1.id, aws_subnet.private_subet_2.id]
+# # eks cluster
+# resource "aws_eks_node_group" "particle41_eks_node_grop" {
+#   cluster_name    = aws_eks_cluster.particle41_eks.name
+#   node_group_name = "particle41_eks_node_grop"
+#   node_role_arn   = aws_iam_role.eks_node_role.arn 
+#   subnet_ids      = [aws_subnet.private_subet_1.id, aws_subnet.private_subet_2.id]
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 1
+#     min_size     = 1
+#   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_worker_node_policy,
-    aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.ec2_container_registry_readonly_policy,
-  ]
-}
+#   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+#   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks_worker_node_policy,
+#     aws_iam_role_policy_attachment.eks_cni_policy,
+#     aws_iam_role_policy_attachment.ec2_container_registry_readonly_policy,
+#   ]
+# }
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,24 +1,24 @@
-# security group for app
-resource "aws_security_group" "allow_http_sg" {
-  vpc_id      = aws_vpc.particle41_vpc.id 
-  description = "security group that allows http connection"
+# # security group for app
+# resource "aws_security_group" "allow_http_sg" {
+#   vpc_id      = aws_vpc.particle41_vpc.id 
+#   description = "security group that allows http connection"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
   
-  tags = {
-    Name = "allow_http_sg"
-  }
-}
+#   tags = {
+#     Name = "allow_http_sg"
+#   }
+# }
 
