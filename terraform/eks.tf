687fe92871304acc64661bbdc3436349ac818c9a
jay415404 <jay416505@gmail.com>
2025-04-19 22:30:25 +0530

    fix: error while attempting to bind on address ('0.0.0.0', 80): permission denied

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -49,9 +49,9 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = [aws_subnet.private_subet_a.id, aws_subnet.private_subet_b.id]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
  update_config {
