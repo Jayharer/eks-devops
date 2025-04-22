beeca1339ea5079669d5203de6a983c2761caf19
jay415404 <jay416505@gmail.com>
2025-04-22 09:33:04 +0530

    update: ecrrepourl output syntax

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -6,11 +6,3 @@ resource "aws_ecr_repository" "particle41_ecr_repo" {
     scan_on_push = true
   }
 }
-
-output "AWS_ACCOUNT_ID" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "AWS_ECR_REPO_NAME" {
-  value = aws_ecr_repository.particle41_ecr_repo.name
-}
---
+++ terraform/outputs.tf
@@ -0,0 +1,8 @@
+// outputs.tf
+output "infrastructure_output" {
+  value = jsonencode({
+    aws_account_id  = data.aws_caller_identity.current.account_id
+    ecr_repo_name = aws_ecr_repository.particle41_ecr_repo.name
+    ecr_repo_url = aws_ecr_repository.particle41_ecr_repo.repository_url
+  })
+}
